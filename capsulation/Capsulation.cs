e70a16db827ffca562ec3a1a831c43ad2777c99f
Gökhan Özcan <gokhanozcan87@gmail.com>
2022-07-22 10:20:41 +0300

    Add project files.

---
+++ capsulation/Capsulation.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace capsulation
+{
+     class Capsulation
+    {
+        private string username = "root", password = "admin", mark = new string('-',6), LoginName;
+            public string checkName
+        {
+            get
+            {
+                return username;
+            }
+        }
+        public string checkPassword
+        {
+            get
+            {
+                return password;
+            }
+        }
+        public string setLoginName
+        {
+            set
+            {
+                LoginName = value;
+            }
+        }
+        public void dbLogin()
+        {
+            Console.WriteLine("{0}\n< Database Bağlantısı Oluştu >\n{1}", mark, mark);
+            Console.WriteLine("-> Bağlantı sağlayan kişi: {0}\n-> Bağlantı sağlanan zaman : {1}\n{2}", LoginName, DateTime.Now.ToString("dd/MM/yyyy HH:mm"), mark);
+        }
+
+    }
+}
---
+++ capsulation/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace capsulation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string userName, userPassword;
+            Capsulation dbManager = new Capsulation();
+            Console.WriteLine("{0}\n< C# Kapsülleme Örnekleri #1 >\n{1}");
+            Console.Write("-> İsminizi Girin: ");
+            dbManager.setLoginName = Console.ReadLine();
+            Console.Write("{0}\n-> Lütfen veritabanı kullanıcı adını girin: ");
+            userName = Console.ReadLine();
+            Console.Write("-> Lütfen veritabanı kullanıcı şifresini girin: ");
+            userPassword = Console.ReadLine();
+            if (dbManager.checkName == userName && dbManager.checkPassword == userPassword)
+            {
+                dbManager.dbLogin();
+            }
+            else
+                Console.WriteLine("{0}\n-> Giriş gerçekleştirilemedi.\n{1}");
+
+        }
+    }
+}
